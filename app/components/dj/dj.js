730d44376449a4788a1255cbbd458b001ab33352
JC <thompsonrell@gmail.com>
2019-04-18 16:53:08 -0400

    working auth page

---
+++ app/components/dj/dj.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class NewsComponent extends Component {
+    render() {
+
+        return (
+            <View>
+                <Text>Hello I'm the Dj</Text>
+            </View>
+        );
+    }
+}
+
+// const styles = StyleSheet.create({
+
+// });
+
+export default NewsComponent;
---
+++ app/components/main/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class GameComponent extends Component {
+    render() {
+
+        return (
+            <View>
+                <Text>Hello I'm the Game Page</Text>
+            </View>
+        );
+    }
+}
+
+// const styles = StyleSheet.create({
+
+// });
+
+export default GameComponent;
